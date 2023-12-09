242e1175682739c0c91963dd5c936dc7a7003d5a
xMarlboro <614049267@qq.com>
2023-12-09 19:48:21 +0800

    第一次提交

---
+++ flutter_application_1/lib/main222.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  //1.runAPP
+  runApp(MaterialApp(
+      home:
+          //Scaffold页面脚手架
+          Scaffold(
+    appBar: AppBar(
+      title: Text('我是标题！'),
+    ),
+    //侧边栏
+  )));
+}
---
+++ flutter_application_1/lib/test1.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+
+void main(List<String> args) {
+  Widget build(BuildContext context) {
+    return Echo(text: "hello world");
+  }
+}
+
+class Echo extends StatelessWidget {
+  const Echo({
+    Key? key,
+    required this.text,
+    this.backgroundColor = Colors.grey, //默认为灰色
+  }) : super(key: key);
+
+  final String text;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: backgroundColor,
+        child: Text(text),
+      ),
+    );
+  }
+}
