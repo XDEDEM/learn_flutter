d189c626f50892309ab4f8cedb5728f6b87db395
xMarlboro <614049267@qq.com>
2023-12-09 21:28:33 +0800

    first push

--- flutter_application_1/lib/main222.dart
+++ flutter_application_1/lib/main222.dart
@@ -7,7 +7,7 @@ void main(List<String> args) {
           //Scaffold页面脚手架
           Scaffold(
     appBar: AppBar(
-      title: Text('我是标题！'),
+      title: Text('第一次改动！'),
     ),
     //侧边栏
   )));
